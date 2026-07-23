70e9235de52da2d75eceb93a0e6cbea037bc027d
ANSAN222 <minshanzhang7@gmail.com>
2026-07-23 17:59:09 +0800

    Add build batch script

---
+++ build.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Installing dependencies...
+pip install -r requirements.txt
+pip install pyinstaller
+
+echo Building executable...
+pyinstaller DesktopPet.spec
+
+echo Build complete! Executable is in dist\DesktopPet.exe
+pause
